732b39d86d26a814498db323f12c41d9b1175f7a
Tristan Hörmann <tristan.hoermann@edu.fhdw.de>
2023-05-17 16:10:59 +0200

    println statements verbessert.

--- src/serverside/alu.rs
+++ src/serverside/alu.rs
@@ -27,6 +27,7 @@ impl Alu {
     /// Soweit alle OpCodes richtig gesetzt sind und ein zulässiger op_code übergeben wird, wird immer ein Ergebnis berechnet.
     /// Sollten OpCodes falsch gesetzt sein, kann fälschlicherweise `0` berechnet werden.
     pub fn calculate(&mut self, op_code: FheUint8, a: FheUint8, b: FheUint8) -> FheUint8 {
+        println!("[ALU] Berechnung gestartet.");
         // Addition
         let is_addition: FheUint8 = op_code.eq(&self.opcode_add);
         let addition = (&a + &b + &self.carry_flag) * is_addition;
@@ -52,7 +53,7 @@ impl Alu {
         self.set_overflow(a.clone(), b.clone(), result.clone());
         self.set_carry(a.clone(), b.clone());
 
-        println!("Alu Berechnung abgeschlossen.");
+        println!("[ALU] Berechnung und Flags abgeschlossen.");
         result
     }
 
--- src/serverside/control_unit.rs
+++ src/serverside/control_unit.rs
@@ -65,15 +65,15 @@ impl ControlUnit {
 
     /// Führt die Fetch, Decode, execute, writeback Zyklen für die übergebene Anzahl an Zyklen aus.
     pub fn start(&mut self, cycles: u8) {
-        println!("CU gestartet.");
+        println!("[ControlUnit] CU gestartet.");
         // Weil das Programm im cipherspace nicht terminieren kann, erstmal fixe cycles laufen lassen.
         for i in 0..cycles {
-            println!("CU-Zyklus {} gestartet.", i);
+            println!("\n[ControlUnit] Zyklus {} gestartet.", i);
             let memory_cell: (FheUint8, FheUint8) = self.memory.read_from_ram(&self.program_counter);
             let opcode: FheUint8 = memory_cell.0;
             let operand: FheUint8 = memory_cell.1;
             let accu: &FheUint8 = &self.memory.get_accu().clone();
-            println!("CU Operanden und Accu ausgelesen.");
+            println!("[ControlUnit] Operanden und Accu ausgelesen.");
 
             // Boolscher Wert für "der akku bekommt einen neuen Wert"
             let is_write_alu_to_accu: FheUint8 = opcode.eq(&self.op_alu_add)
@@ -83,11 +83,11 @@ impl ControlUnit {
             let is_write_value_to_accu: FheUint8 = opcode.eq(&self.op_load);
 
             let is_write_accu: FheUint8 = &is_write_alu_to_accu | &is_write_value_to_accu;
-            println!("CU IsWriteAccu ausgewertet.");
+            println!("[ControlUnit] IsWriteAccu ausgewertet.");
 
             // Boolscher Wert für "der ram bekommt einen neuen Wert"
             let is_write_ram: FheUint8 = opcode.eq(&self.op_save);
-            println!("CU IsWriteRam ausgewertet.");
+            println!("[ControlUnit] IsWriteRam ausgewertet.");
 
             // Akku-Wert an die Adresse OPERAND shreiben, wenn geschrieben werden soll.
             self.memory.write_to_ram(
@@ -95,10 +95,10 @@ impl ControlUnit {
                 accu.clone(),
                 &is_write_ram,
             );
-            println!("CU möglichen Schreibzugriff im RAM getätigt");
+            println!("[ControlUnit] möglichen Schreibzugriff im RAM getätigt");
 
             let alu_result = self.alu.calculate(opcode.clone(), operand.clone(), accu.clone());
-            println!("CU mögliches ALU Ergebnis bestimmt.");
+            println!("[ControlUnit] mögliches ALU Ergebnis bestimmt.");
 
             let possible_new_accu_value = alu_result * is_write_alu_to_accu + operand.clone() * is_write_value_to_accu;
 

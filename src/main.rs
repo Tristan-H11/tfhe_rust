74bfcb77396b34a582d7a4059890e7ff19e7da8e
tristanhormann <triddel02@gmx.de>
2023-05-22 10:07:00 +0200

    Ausführungsdauer für Client, Server und Verify Gesamt-Ausführung ergänzt.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::time::Instant;
 use crate::clientside::{client, verify};
 use crate::serverside::server;
 
@@ -8,16 +9,21 @@ mod serverside;
 /// Später müssen die drei `start()` Funktionen die Main-Funktionen der entsprechenden
 /// Client- oder Server-Programme werden.
 fn main() {
+    let start = Instant::now();
     client::start().expect("Fehler im Client!");
     println!("-------");
-    println!("Client Ausführung beendet!");
+    println!("Client Ausführung beendet! Zeit in ms: {}", (start.elapsed()).as_millis());
     println!("-------");
+
+    let start = Instant::now();
     server::start().expect("Fehler im Server!");
     println!("-------");
-    println!("Server Ausführung beendet!");
+    println!("Server Ausführung beendet! Zeit in ms: {}", (start.elapsed()).as_millis());
     println!("-------");
+
+    let start = Instant::now();
     verify::start().expect("Fehler im Verify!");
     println!("-------");
-    println!("Verify Ausführung beendet!");
+    println!("Verify Ausführung beendet! Zeit in ms: {}", (start.elapsed()).as_millis());
     println!("-------");
 }

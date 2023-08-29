1fa0ae880ad3ba1ffd4786e1dc1296e38c0fa654
tristanhormann <triddel02@gmx.de>
2023-08-29 13:51:16 +0200

    statics in binär umgeschrieben.

--- src/clientside/statics.rs
+++ src/clientside/statics.rs
@@ -3,25 +3,25 @@
 // Befehl verglichen werden muss. Somit müssen die OpCodes nicht trivial verschlüsselt werden.
 
 // Setzen der OpCodes
-pub static ALU_ADD: u8  = 1; // Addition
-pub static ALU_OR: u8   = 2; // Bitwise Or
-pub static ALU_AND: u8  = 3; // Bitwise And
-pub static ALU_XOR: u8  = 4; // Bitwise Xor
-pub static ALU_SUB: u8  = 5; // Subtraction
-pub static ALU_MUL: u8  = 6; // Multiplication
+pub static ALU_ADD: u8  = 0b0000_0001; // Addition
+pub static ALU_OR: u8   = 0b0000_0010; // Bitwise Or
+pub static ALU_AND: u8  = 0b0000_0011; // Bitwise And
+pub static ALU_XOR: u8  = 0b0000_0100; // Bitwise Xor
+pub static ALU_SUB: u8  = 0b0000_0101; // Subtraction
+pub static ALU_MUL: u8  = 0b0000_0110; // Multiplication
 
-pub static ALU_ADD_R: u8  = 7; // Addition
-pub static ALU_OR_R: u8   = 8; // Bitwise Or
-pub static ALU_AND_R: u8  = 9; // Bitwise And
-pub static ALU_XOR_R: u8  = 10; // Bitwise Xor
-pub static ALU_SUB_R: u8  = 11; // Subtraction
-pub static ALU_MUL_R: u8  = 12; // Multiplication
+pub static ALU_ADD_R: u8  = 0b0000_0111; // Addition
+pub static ALU_OR_R: u8   = 0b0000_1000; // Bitwise Or
+pub static ALU_AND_R: u8  = 0b0000_1001; // Bitwise And
+pub static ALU_XOR_R: u8  = 0b0000_1010; // Bitwise Xor
+pub static ALU_SUB_R: u8  = 0b0000_1011; // Subtraction
+pub static ALU_MUL_R: u8  = 0b0000_1100; // Multiplication
 
-pub static LOAD: u8     = 13; // Konstante in Accu laden
-pub static LOAD_R: u8   = 14; // Wert von RAM Adr in Accu laden
-pub static SAVE: u8     = 15; // Accu an RAM Adr speichern
+pub static LOAD: u8     = 0b0000_1101; // Konstante in Accu laden
+pub static LOAD_R: u8   = 0b0000_1110; // Wert von RAM Adr in Accu laden
+pub static SAVE: u8     = 0b0000_1111; // Accu an RAM Adr speichern
 
-pub static JNZ: u8  = 16; // Jump if not zero
+pub static JNZ: u8  = 0b0001_0000; // Jump if not zero
 
 // Damit der PC und die Flags etc im Server nicht trivial verschlüsselt werden muss
 pub static ZERO_INITIALIZER: u8 = 0;

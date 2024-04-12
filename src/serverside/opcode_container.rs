13c9e9c722bf3e31666f2580207e706926f95861
Tristan-H11 <triddel02@gmx.de>
2024-04-12 13:39:18 +0200

    OpCodeContainer um doku erweitert

--- src/serverside/opcode_container.rs
+++ src/serverside/opcode_container.rs
@@ -3,7 +3,9 @@ use tfhe::prelude::*;
 use tfhe::FheUint8;
 use crate::encrypt_trivial;
 
-/// Datenstruktur zum Speichern aller Opcodes und ausführen einfacher inhaltlicher Abfragen.
+///
+/// Datenstruktur zum Speichern aller Opcodes und Ausführen einfacher inhaltlicher Abfragen.
+///
 #[derive(Clone)]
 pub struct OpcodeContainer {
     pub(crate) opcodes_alu: OpcodeContainerAlu,
@@ -14,6 +16,11 @@ pub struct OpcodeContainer {
 }
 
 impl OpcodeContainer {
+    ///
+    /// Erzeugt einen neuen OpcodeContainer.
+    ///
+    /// # Returns
+    /// * Einen neuen OpcodeContainer.
     pub fn new() -> OpcodeContainer {
         let opcodes_alu = OpcodeContainerAlu::new();
 
@@ -25,28 +32,63 @@ impl OpcodeContainer {
             jnz: encrypt_trivial!(0b0010_0001u8),
         }
     }
+    ///
     /// Prüft, ob es sich um einen Command handelt, welcher einen Wert aus dem RAM in den Akkumulator laden soll.
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen Load-Command handelt, sonst `0`.
     pub fn is_load_command(&self, opcode: &FheUint8) -> FheUint8 {
         opcode.eq(&self.load) | opcode.eq(&self.load_r)
     }
 
+    ///
     /// Prüft, ob es sich um einen Command handelt, welcher einen Befehl aus dem RAM laden muss.
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen Load-Operand-Command handelt, sonst `0`.
     pub fn has_to_load_operand_from_ram(&self, opcode: &FheUint8) -> FheUint8 {
         self.opcodes_alu.is_ram_opcode(opcode) | self.load_r.eq(opcode)
     }
 
+    ///
     /// Prüft, ob es sich um einen Command handelt, welcher eine ALU-Berechnung auslösen soll.
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen ALU-Command handelt, sonst `0`.
     pub fn is_alu_command(&self, opcode: &FheUint8) -> FheUint8 {
         let alu_mask: &FheUint8 = &encrypt_trivial!(0b1000_0000u8);
         (opcode & alu_mask).eq(alu_mask)
     }
 
+    ///
     /// Prüft, ob es sich um einen Command handelt, welcher einen Wert in den RAM schreiben soll.
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen Store-Command handelt, sonst `0`.
     pub fn is_write_to_ram(&self, opcode: &FheUint8) -> FheUint8 {
         opcode.eq(&self.store)
     }
 
+    ///
     /// Prüft, ob es sich um einen Sprungbefehl handelt.
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen Sprungbefehl handelt, sonst `0`.
     pub fn is_jump_command(&self, opcode: &FheUint8) -> FheUint8 {
         opcode.eq(&self.jnz)
     }
--- src/serverside/opcode_container_alu.rs
+++ src/serverside/opcode_container_alu.rs
@@ -2,7 +2,9 @@ use tfhe::prelude::*;
 use tfhe::FheUint8;
 use crate::encrypt_trivial;
 
+///
 /// Datenstruktur zum Speichern aller ALU-Opcodes und Ausführen einfacher inhaltlicher Abfragen.
+///
 #[derive(Clone)]
 pub struct OpcodeContainerAlu {
     pub(crate) add: FheUint8,
@@ -43,17 +45,38 @@ impl OpcodeContainerAlu {
 }
 
 impl OpcodeContainerAlu {
+    ///
     /// Prüft, ob der OpCode einen ALU-Befehl repräsentiert
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen ALU-Command handelt, sonst `0`.
     pub fn contains_opcode(&self, opcode: &FheUint8) -> FheUint8 {
         self.is_ram_opcode(opcode) | self.is_constant_opcode(opcode)
     }
 
+    ///
     /// Prüft, ob es sich um einen ALU-Opcode handelt, welcher einen Wert aus dem RAM auslesen muss
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen ALU-Command handelt, welcher einen Wert aus dem RAM auslesen muss, sonst `0`.
     pub fn is_ram_opcode(&self, opcode: &FheUint8) -> FheUint8 {
         (opcode & &self.alu_ram_mask).eq(&self.alu_ram_mask)
     }
 
+    ///
     /// Prüft, ob es sich um einen ALU-Opcode handelt, welcher einen Wert als Konstante erwartet.
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen ALU-Command handelt, welcher einen Wert als Konstante erwartet, sonst `0`.
     pub fn is_constant_opcode(&self, opcode: &FheUint8) -> FheUint8 {
         let one: FheUint8 = encrypt_trivial!(1u8);
         let msb_equal = (opcode & &self.alu_const_mask).eq(&self.alu_const_mask);
@@ -61,26 +84,74 @@ impl OpcodeContainerAlu {
         msb_equal.eq(not_ram_flag) // (Erstes Bit gesetzt) == (zweites Bit nicht gesetzt)
     }
 
+    ///
+    /// Prüft, ob es sich um einen Additions-Opcode handelt.
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen Additions-Opcode handelt, sonst `0`.
     pub fn is_add(&self, opcode: &FheUint8) -> FheUint8 {
         opcode.eq(&self.add) | opcode.eq(&self.add_r)
     }
 
+    ///
+    /// Prüft, ob es sich um einen And-Opcode handelt.
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen And-Opcode handelt, sonst `0`.
     pub fn is_and(&self, opcode: &FheUint8) -> FheUint8 {
         opcode.eq(&self.and) | opcode.eq(&self.and_r)
     }
 
+    ///
+    /// Prüft, ob es sich um einen Or-Opcode handelt.
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen Or-Opcode handelt, sonst `0`.
     pub fn is_or(&self, opcode: &FheUint8) -> FheUint8 {
         opcode.eq(&self.or) | opcode.eq(&self.or_r)
     }
 
+    ///
+    /// Prüft, ob es sich um einen Xor-Opcode handelt.
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen Xor-Opcode handelt, sonst `0`.
     pub fn is_xor(&self, opcode: &FheUint8) -> FheUint8 {
         opcode.eq(&self.xor) | opcode.eq(&self.xor_r)
     }
 
+    ///
+    /// Prüft, ob es sich um einen Subtraktions-Opcode handelt.
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen Subtraktions-Opcode handelt, sonst `0`.
     pub fn is_sub(&self, opcode: &FheUint8) -> FheUint8 {
         opcode.eq(&self.sub) | opcode.eq(&self.sub_r)
     }
 
+    ///
+    /// Prüft, ob es sich um einen Multiplikations-Opcode handelt.
+    ///
+    /// # Arguments
+    /// * `opcode` - Der zu prüfende OpCode.
+    ///
+    /// # Returns
+    /// * `1`, wenn es sich um einen Multiplikations-Opcode handelt, sonst `0`.
     pub fn is_mul(&self, opcode: &FheUint8) -> FheUint8 {
         opcode.eq(&self.mul) | opcode.eq(&self.mul_r)
     }
